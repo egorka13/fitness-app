f097fac1cff018c7d87d02dee5bc599ac654ae5e
egorka13 <shir-egor@yandex.ru>
2025-01-30 23:02:37 +0100

    move code to separate component

--- src/components/ExerciseDetails/ExerciseDetailsHistoryTable/ExerciseDetailsHistoryTable.tsx
+++ src/components/ExerciseDetails/ExerciseDetailsHistoryTable/ExerciseDetailsHistoryTable.tsx
@@ -9,14 +9,7 @@ import {
   ExerciseRecord,
 } from './types';
 import { useAuth } from '../../../context/AuthContext';
-import {
-  AreaChart,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Area,
-  Tooltip,
-} from 'recharts';
+import { ExerciseDetailsGraph } from '../ExerciseDetailsGraph/ExerciseDetailsGraph';
 
 export const ExerciseDetailsHistoryTable: React.FC<
   ExerciseDetailsHistoryTableProps
@@ -75,14 +68,6 @@ export const ExerciseDetailsHistoryTable: React.FC<
     // eslint-disable-next-line
   }, []);
 
-  const graphData = [...records]
-    .sort((a, b) => a.key - b.key)
-    .map((record, index) => ({
-      name: index + 1,
-      uv: record.weight,
-      // pv: record.repeats,
-    }));
-
   return (
     <ConfigProvider
       theme={{
@@ -103,43 +88,7 @@ export const ExerciseDetailsHistoryTable: React.FC<
     >
       {contextHolder}
 
-      {records.length >= 2 ? (
-        <AreaChart
-          width={document.body.clientWidth}
-          height={200}
-          data={graphData}
-          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-        >
-          <defs>
-            <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-            </linearGradient>
-            <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-              <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
-              <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
-            </linearGradient>
-          </defs>
-          <XAxis dataKey="name" />
-          <YAxis />
-          <CartesianGrid strokeDasharray="3 3" />
-          <Tooltip />
-          <Area
-            type="monotone"
-            dataKey="uv"
-            stroke="#8884d8"
-            fillOpacity={1}
-            fill="url(#colorUv)"
-          />
-          <Area
-            type="monotone"
-            dataKey="pv"
-            stroke="#82ca9d"
-            fillOpacity={1}
-            fill="url(#colorPv)"
-          />
-        </AreaChart>
-      ) : null}
+      {records.length >= 2 ? <ExerciseDetailsGraph data={records} /> : null}
 
       <div className={styles.tableContainer}>
         <Table dataSource={records} columns={columns} pagination={false} />
